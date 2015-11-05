ae4702f8b35357e9169417d681fff9cfcc097de6
ElenaLopez_Medianet_es <elena.lopez@medianet.es>
2015-11-05 09:08:25 +0100

    ShortURL repository created

---
+++ Scenarios_Test/screens/main_page_screen.rb
@@ -0,0 +1,46 @@
+class MainPageScreen
+
+	SCREEN_HEADER = {
+		web: "mainPage"
+	}
+
+	URL_FIELD = {
+		web: "{ type: "input", expression: {id: "basic-url"}}"
+	}
+
+	GO_BUTTON = {
+		web: "web: { type: "button", expression: {class: 'input-group-btn'}}"
+	}
+
+def initialize
+    super SCREEN_HEADER
+end 
+
+def wait_for_screen(timeout = 20)
+    alert_saved = false
+    puts "Waiting for screen of type #{self.class}"
+    while (timeout > 0)
+      if (screen_exists?)
+        puts "Screen Found!!!"
+        # Wait until the CRM image appears
+        trace_sleep(5) if is_android?
+        return
+      end
+      alert_saved = check_alerts alert_saved
+      trace_sleep(1)
+      timeout -= 1
+    end
+    fail("Timeout waiting for screen of type #{self.class}")
+end
+
+def fill_url_field(url)
+ 	fill(URL_FIELD, url)
+end
+
+def touch_go_button
+	tap(GO_BUTTON)
+end
+
+def are_url_shorter(text)
+	predicate_contains(@selector,text)
+end
